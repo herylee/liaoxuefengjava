5dcfabaeaa1be317b614805c05949daa3688bc6e
hery186 <2940045321@qq.com>
2018-05-08 23:43:47 +0800

    5-8   23:43  反射 泛型  使用注解

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,42 @@
-	
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-/***    Person p = new Student("xiao ming ");
-		System.out.println(p.hello());  //打印出来的是引用地址
-		Student s = new Student("xiao ming ");
-//		p.run();
-//		s.run();
-***/
-//		Shape s1 =  new Rect(200,100);
-//		Shape s2 = new Circle(60);
-//		System.out.println(s1.area());
-//		System.out.println(s2.area());
-//		
-		
-		Person p1 = new Person("xiaoming");
-		System.out.println(Person.getNumber());
-		
-		Person p2 = new Person("xiaoming");
-		System.out.println(Person.getNumber());
-		
-		Person p3 = new Person("xiaoming");
-		System.out.println(Person.getNumber());
-		
-		
-	
+		@SuppressWarnings({ "rawtypes", "unused" })
+		Class cls = Main.class;
+		Main.hello();
 	}
 
-	// System.out.println(s.hello());
+	
+	@Deprecated 
+	public static void hello() {	
+		
+	}
+	@Override
+	public String toString() {
+
+		return "Main";
+	}
 
 }
+
+/***
+ * Person p = new Student("xiao ming "); System.out.println(p.hello());
+ * //打印出来的是引用地址 Student s = new Student("xiao ming "); // p.run(); // s.run();
+ ***/
+// Shape s1 = new Rect(200,100);
+// Shape s2 = new Circle(60);
+// System.out.println(s1.area());
+// System.out.println(s2.area());
+//
+
+// Person p1 = new Person("xiaoming");
+// System.out.println(Person.getNumber());
+//
+// Person p2 = new Person("xiaoming");
+// System.out.println(Person.getNumber());
+//
+// Person p3 = new Person("xiaoming");
+// System.out.println(Person.getNumber());
+//
--- src/Student.java
+++ src/Student.java
@@ -1,37 +1,20 @@
 
-public class Student extends Person {
-
-	public Student(String name) {
-		super(name);
-		// TODO Auto-generated constructor stub
-	}
-
-	private int score;
-
-	public int getScore() {
-
-		return score;
-
-	}
-
-	public void setScore(int score) {
-
-		this.score = score;
-
-	}
-
-	public void run() {
-
-		System.out.println("student" + getName() + "is running!");
-
-	}
+public class Student implements  Hello {
 
 	
-	//覆写了父类的方法
-	public String hello() {
-
-		return super.hello()+"!";
-
-	}
+		private String name;
+		
+		public Student() {
+			
+			this("unnamed");
+		}
+	
+		public Student(String name) {
+			
+			this.name=name;
+		}
+	
+	
+ 
 
 }
