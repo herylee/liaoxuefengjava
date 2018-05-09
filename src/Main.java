bd46bd423da887fed1fb4d4f4e7e30c494d507e5
hery <2940045321@qq.com>
2018-05-09 09:07:09 +0800

    7

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,42 @@
-	
+import java.beans.BeanInfo;
+
 public class Main {
 
-	public static void main(String[] args) {
+	/***
+	 * Person p = new Student("xiao ming "); System.out.println(p.hello());
+	 * //打印出来的是引用地址 Student s = new Student("xiao ming "); // p.run(); // s.run();
+	 ***/
+	// Shape s1 = new Rect(200,100);
+	// Shape s2 = new Circle(60);
+	// System.out.println(s1.area());
+	// System.out.println(s2.area());
+	//
+	//
+	// Person p1 = new Person("xiaoming");
+	// System.out.println(Person.getNumber());
+	//
+	// Person p2 = new Person("xiaoming");
+	// System.out.println(Person.getNumber());
+	//
+	// Person p3 = new Person("xiaoming");
+	// System.out.println(Person.getNumber());
+	//
+//
+//	String name = "World";
+//	StringBuilder sb = new StringBuilder();
+//
+//	sb.append("hello,  ").append(name).append('!');
+//	String s = sb.toString();
+//	System.out.println(s);
 
-/***    Person p = new Student("xiao ming ");
-		System.out.println(p.hello());  //打印出来的是引用地址
-		Student s = new Student("xiao ming ");
-//		p.run();
-//		s.run();
-***/
-//		Shape s1 =  new Rect(200,100);
-//		Shape s2 = new Circle(60);
-//		System.out.println(s1.area());
-//		System.out.println(s2.area());
-//		
-		
-		Person p1 = new Person("xiaoming");
-		System.out.println(Person.getNumber());
-		
-		Person p2 = new Person("xiaoming");
-		System.out.println(Person.getNumber());
-		
-		Person p3 = new Person("xiaoming");
-		System.out.println(Person.getNumber());
-		
-		
 	
+	public static void main(String[] args) throws Exception{
+
+
+		BeanInfo bi = Introspector.getBeanInfo(Person.class);
+		for(Property)
+		
+		
 	}
 
 	// System.out.println(s.hello());
--- src/Person.java
+++ src/Person.java
@@ -1,64 +1,108 @@
 
 public class Person {
 
+//	private String name;
+//	private int age;
+//
+//	
+//	private	static int number;
+//	
+//	
+//	public Person(String name) {
+//		this.name = name;
+//		number++;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public int getAge() {
+//
+//		return age;
+//
+//	}
+//
+//	public void setAge(int age) {
+//
+//		this.age = age;
+//
+//	}
+//
+//	public String hello() {
+//		
+//		return "hello, "+this.name;
+//		
+//	}
+//	
+//	
+//	
+//	
+//	public void run() {
+//
+//		System.out.println(name + "is aaa!");
+//
+//	}
+//
+//	public String toString() {
+//
+//		return "preson " + this.name;
+//
+//	}
+//
+//	
+//	public static int getNumber() {
+//		
+//		return number;
+//	}
+//	
+	
+	
 	private String name;
 	private int age;
-
+	private boolean gender;
+	private String address;
 	
-	private	static int number;
 	
 	
-	public Person(String name) {
-		this.name = name;
-		number++;
+	public int getAge() {
+		return age;
 	}
-
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public boolean isGender() {
+		return gender;
+	}
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 	public String getName() {
 		return name;
 	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-
-		return age;
-
-	}
-
-	public void setAge(int age) {
-
-		this.age = age;
-
-	}
-
-	public String hello() {
-		
-		return "hello, "+this.name;
-		
-	}
 	
 	
 	
 	
-	public void run() {
-
-		System.out.println(name + "is aaa!");
-
-	}
-
-	public String toString() {
-
-		return "preson " + this.name;
-
-	}
-
 	
-	public static int getNumber() {
-		
-		return number;
-	}
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
